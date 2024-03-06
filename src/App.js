f88c615ce6b06c72ed2c17444e09fa3b00964921
Yaswanth791504 <mallisettiyaswath@gmail.com>
2024-03-06 11:07:05 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FaceDetectionComponent from "./components/FaceDetection";
+
+function App() {
+  return (
+    <div className="App">
+      <FaceDetectionComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/newOne.js
@@ -0,0 +1,195 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import * as handpose from "@tensorflow-models/handpose";
+import "@tensorflow/tfjs-backend-webgl";
+import * as faceapi from "face-api.js";
+import React, { useEffect, useRef, useState } from "react";
+
+const FaceDetectionComponent = () => {
+  const videoRef = useRef();
+  const [detectedFaces, setDetectedFaces] = useState(0);
+  const [faceTilt, setFaceTilt] = useState("");
+  const [faceOrientation, setFaceOrientation] = useState("");
+  const [mouthStatus, setMouthStatus] = useState("");
+  const [handRaised, setHandRaised] = useState(false); // State to track if a hand is raised
+
+  useEffect(() => {
+    const loadModels = async () => {
+      await Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+        handpose.load(), // Load the handpose model
+      ]);
+      console.log("Models loaded");
+    };
+
+    const startVideo = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
+        videoRef.current.srcObject = stream;
+      } catch (error) {
+        console.error("Error accessing the webcam", error);
+      }
+    };
+
+    loadModels().then(startVideo);
+
+    return () => {
+      if (videoRef.current && videoRef.current.srcObject) {
+        videoRef.current.srcObject.getTracks().forEach((track) => track.stop());
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    const detectFacesAndHands = async () => {
+      if (!videoRef.current) return;
+
+      const canvas = faceapi.createCanvasFromMedia(videoRef.current);
+      document.body.append(canvas);
+      const displaySize = {
+        width: videoRef.current.width,
+        height: videoRef.current.height,
+      };
+      faceapi.matchDimensions(canvas, displaySize);
+
+      const handModel = await handpose.load(); // Make sure the model is loaded
+
+      const interval = setInterval(async () => {
+        const faces = await faceapi
+          .detectAllFaces(
+            videoRef.current,
+            new faceapi.TinyFaceDetectorOptions()
+          )
+          .withFaceLandmarks()
+          .withFaceExpressions();
+
+        const hands = await handModel.estimateHands(videoRef.current, true);
+        setHandRaised(
+          hands.length > 0 &&
+            hands.some((hand) => hand.handInViewConfidence > 0.8)
+        ); // Update based on confidence
+
+        const resizedDetections = faceapi.resizeResults(faces, displaySize);
+        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+        analyzeFaceFeatures(resizedDetections);
+
+        // You could enhance this by checking the position of the hand to see if it's actually raised
+      }, 100);
+
+      return () => {
+        clearInterval(interval);
+      };
+    };
+
+    videoRef.current &&
+      videoRef.current.addEventListener("play", detectFacesAndHands);
+  }, []);
+
+  // Your existing analyzeFaceFeatures function goes here
+  const analyzeFaceFeatures = (detections) => {
+    if (detections.length > 1) {
+      alert("2 or more faces detected. Focusing on the nearest.");
+    }
+
+    const sortedDetections = detections.sort((a, b) => {
+      const aArea = a.detection.box.width * a.detection.box.height;
+      const bArea = b.detection.box.width * b.detection.box.height;
+      return bArea - aArea;
+    });
+
+    const detection = sortedDetections[0];
+    if (detection) {
+      const landmarks = detection.landmarks;
+      const jawOutline = landmarks.getJawOutline();
+      const nose = landmarks.getNose();
+
+      const faceTilt = detectFaceTilt(jawOutline);
+      const faceOrientation = detectFaceOrientation(jawOutline, nose);
+      const mouthStatus = detectMouthStatus(landmarks.getMouth());
+
+      setDetectedFaces(detections.length);
+      setFaceOrientation(faceOrientation);
+      setFaceTilt(faceTilt);
+      setMouthStatus(mouthStatus);
+
+      console.log(
+        `Nearest Face: Tilt - ${faceTilt}, Orientation - ${faceOrientation}, Mouth - ${mouthStatus}`
+      );
+    }
+  };
+
+  function detectFaceTilt(jawOutline) {
+    const leftSide = jawOutline.slice(0, 8);
+    const rightSide = jawOutline.slice(8, 17);
+    const leftY = leftSide.reduce((acc, p) => acc + p.y, 0) / leftSide.length;
+    const rightY =
+      rightSide.reduce((acc, p) => acc + p.y, 0) / rightSide.length;
+
+    if (Math.abs(leftY - rightY) < 30) {
+      return "Forward";
+    }
+    return leftY > rightY ? "Right Tilt" : "Left Tilt";
+  }
+
+  function detectFaceOrientation(jawOutline, nose) {
+    const jawLeftPoint = jawOutline[0];
+    const jawRightPoint = jawOutline[jawOutline.length - 1];
+    const nosePoint = nose[3];
+
+    const noseToLeftJawDistance = Math.abs(nosePoint.x - jawLeftPoint.x);
+    const noseToRightJawDistance = Math.abs(nosePoint.x - jawRightPoint.x);
+    const totalJawWidth = Math.abs(jawLeftPoint.x - jawRightPoint.x);
+
+    const turnRatioThreshold = 0.7;
+    const leftRatio = noseToLeftJawDistance / totalJawWidth;
+    const rightRatio = noseToRightJawDistance / totalJawWidth;
+
+    if (leftRatio > turnRatioThreshold) {
+      return "Facing Left";
+    } else if (rightRatio > turnRatioThreshold) {
+      return "Facing Right";
+    } else {
+      return "Facing Forward";
+    }
+  }
+
+  function detectMouthStatus(mouth) {
+    const topLip = mouth[13].y;
+    const bottomLip = mouth[19].y;
+    const mouthOpenThreshold = 7;
+    return bottomLip - topLip > mouthOpenThreshold ? "Open" : "Closed";
+  }
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "20px",
+          width: "720px",
+        }}
+      >
+        <span>Detected Faces: {detectedFaces}</span>
+        <span>Face-Tilt: {faceTilt}</span>
+        <span>Face-Orientation: {faceOrientation}</span>
+        <span>Mouth: {mouthStatus}</span>
+        <span>Hand Raised: {handRaised ? "Yes" : "No"}</span>{" "}
+        {/* Display hand raise status */}
+      </div>
+      <video
+        ref={videoRef}
+        width="720"
+        height="560"
+        autoPlay
+        muted
+        style={{ display: "block" }}
+      ></video>
+    </div>
+  );
+};
+export default FaceDetectionComponent;
